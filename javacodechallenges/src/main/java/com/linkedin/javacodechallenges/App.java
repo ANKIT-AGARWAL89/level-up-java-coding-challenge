a420dc60c4e69549159a3a38bcbc421c922f985f
ANKIT AGARWAL <97042031+ANKIT-AGARWAL89@users.noreply.github.com>
2024-03-01 05:46:23 +0000

    implemented double or nothing program

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -1,9 +1,6 @@
 package com.linkedin.javacodechallenges;
 
-/**
- * Hello world!
- *
- */
+ 
 public class App 
 {
     public static void main( String[] args )
--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/DoubleOrNothing.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/DoubleOrNothing.java
@@ -1,6 +1,37 @@
+package com.linkedin.javacodechallenges;
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class DoubleOrNothing {
 
-  public void playGame() {
-    // TODO: Implement method
+   
+    public boolean askChoice(){
+    System.out.println("do you want to play this game or want to exit with 10 points? play = true or do not want to play = false");
+    Scanner sc = new Scanner(System.in);
+    boolean choice = sc.nextBoolean();
+    return choice;
+
   }
+
+  public void playGame() {
+    System.out.println("You have 10 points.");
+    int currentPoints = 10;
+    Random random = new Random();
+    boolean wantToPlay = askChoice();
+    while (wantToPlay) {
+      boolean turn = random.nextBoolean();
+      if(turn){
+        currentPoints*=2;
+        System.out.println("your current points: " + currentPoints);
+        wantToPlay = askChoice();
+      }
+      else{
+        currentPoints*=0;
+        wantToPlay = false; 
+      }
+    }
+    System.out.println("your final points: "+ currentPoints);
+}
+  
 }
