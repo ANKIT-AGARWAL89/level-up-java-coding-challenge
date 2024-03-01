a5660ad2ab1b293771a2b5573082e721b825b5ae
ANKIT AGARWAL <97042031+ANKIT-AGARWAL89@users.noreply.github.com>
2024-03-01 06:54:15 +0000

    implemented the model a person program.

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -2,8 +2,10 @@ package com.linkedin.javacodechallenges;
 
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        
+        Person p1 = new Person("Ankit", "Agarwal");
+        p1.setAge(20);
+        p1.introduction();
     }
 }
--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/Person.java
@@ -1,3 +1,30 @@
+package com.linkedin.javacodechallenges;
+
 public class Person {
-  
+  String first_name;
+  String last_name;
+
+  // get set method
+  private int age;
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  // constructor
+  public Person(String first_name, String last_name) {
+    this.first_name = first_name;
+    this.last_name = last_name;
+  }
+
+  // method for introduction of a person
+  public void introduction() {
+    System.out.println(
+        "My name is " + this.first_name + " " + this.last_name + ". " + "I am " + this.getAge() + " years old.");
+  }
+
 }
