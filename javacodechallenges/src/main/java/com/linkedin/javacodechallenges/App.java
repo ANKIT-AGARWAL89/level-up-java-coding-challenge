8669a32e84542694d80aeeaf8f31de3bbdfbca7f
ANKIT AGARWAL <97042031+ANKIT-AGARWAL89@users.noreply.github.com>
2024-03-01 04:48:23 +0000

    implemented password complexity program

--- javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
+++ javacodechallenges/src/main/java/com/linkedin/javacodechallenges/App.java
@@ -5,7 +5,61 @@ import java.util.Scanner;
 public class App {
 
     public static boolean isPasswordComplex(String password) {
-        return false;
+        // METHOD 1 (My method)
+        // if (password.length() < 6) {
+        // return false;
+        // }
+
+        // boolean hasUpperCase = false;
+        // boolean hasLowerCase = false;
+        // boolean hasDigit = false;
+        // for (int i = 0; i < password.length(); i++) {
+
+        // if (password.charAt(i) <= 90 && password.charAt(i) >= 65) {
+        // hasUpperCase = true;
+        // continue;
+        // }
+
+        // else if (password.charAt(i) <= 122 && password.charAt(i) >= 97) {
+        // hasLowerCase = true;
+        // continue;
+        // }
+
+        // else if (password.charAt(i) <= 57 && password.charAt(i) >= 48) {
+        // hasDigit = true;
+        // }
+
+        // }
+
+        // return hasUpperCase == true && hasLowerCase == true && hasDigit == true;
+
+
+// METHOD 2 (mentor's method)
+        // return password.length() >= 6
+        //         && password.matches(".*\\d.*")
+        //         && password.matches(".*[A-Z].*.")
+        //         && password.matches(".*[a-z].*");
+
+// METHOD 3 (mentor's method)
+         if (password.length() < 6) {
+            return false;
+        }
+        boolean hasUpperCase = false;
+        boolean hasLowerCase = false;
+        boolean hasDigit = false;
+        for(int i=0; i<password.length();i++){
+            char current = password.charAt(i);
+            if(Character.isDigit(current)){
+                hasDigit = true;
+            }
+            else if(Character.isUpperCase(current)){
+                hasUpperCase = true;
+            }
+            else if(Character.isLowerCase(current)){
+                hasLowerCase = true;
+            }
+        }
+        return hasDigit && hasLowerCase && hasUpperCase;
     }
 
     public static void main(String[] args) {
